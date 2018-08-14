12a499104b5b63d465508edad78f4d09a8611672
fefoglino <42367586+fefoglino@users.noreply.github.com>
2018-08-14 17:10:41 -0400

    Create otakustalker_problematicanalysis.py

---
+++ otakustalker_problematicanalysis.py
@@ -0,0 +1,28 @@
+import json
+
+def probFactor(jsonFile):
+    allTheTweets = str()
+
+    tweetFile = open(jsonFile, "r")
+    tweetData = json.load(tweetFile)
+    tweetFile.close()
+
+    words = open("bad-words.txt", "r")
+    wordsList = words.read()
+
+    for i in range(len(tweetData)):
+        allTheTweets+=tweetData[i]["text"]
+
+    allTheTweets=allTheTweets.split() # now we have a list of every word in the tweet batch
+
+    probCounter = 0
+
+    for i in range(len(allTheTweets)):
+        if allTheTweets[i] in wordsList:
+            probCounter+=1
+
+    probFactor = int(probCounter)/int(len(allTheTweets))
+    print(probFactor)
+    return(probFactor)
+
+    words.close()
