863c39d064b52deb975608b95a35cedfad74f1b8
fefoglino <42367586+fefoglino@users.noreply.github.com>
2018-08-15 15:40:07 -0400

    Update otakustalker_tweetfrequency.py

--- otakustalker_tweetfrequency.py
+++ otakustalker_tweetfrequency.py
@@ -1,18 +1,22 @@
 import json
 from datetime import *
 
-jsonFile = "tweets_small.json"
+jsonFiles = "tweets_small.json"
 datetimeList = []
 datetimeDifferenceList = []
 averagelist = []
 
 def tweetFrequency(jsonFile):
+    datetimeList = []
     tweetFile = open(jsonFile, "r")
     tweetData = json.load(tweetFile)
     tweetFile.close()
 
+    datetimeLists = []
     for i in range(len(tweetData)):
         datetimeList.append(datetime.strptime(str(tweetData[i]["created_at"]),"%a %b %d %H:%M:%S %z %Y"))
+
+    datetimeList=sorted(datetimeList)
 
     for i in range(len(datetimeList)):
         datetimeDifferenceList.append(abs(datetimeList[i]-datetimeList[i-1]).seconds/3600)
