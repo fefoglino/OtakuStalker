fe1afbd9ca3f675d5aa10af7f1f1d9fc5a7f1fd2
fefoglino <42367586+fefoglino@users.noreply.github.com>
2018-08-15 09:30:14 -0400

    Update otakustalker-username-analysis.py

--- otakustalker-username-analysis.py
+++ otakustalker-username-analysis.py
@@ -1,9 +1,12 @@
 import otakustalker_wordfreq as wf
 import otakustalker_problematicanalysis as pa
 import fixingHashtagCounter as hc
+import otakustalker_numberoftweets as nt
 
 pa.probFactor("tweets_small.json")
 
 hc.mostCommonHashtags("tweets_small.json")
+
+nt.numberOfTweets("tweets_small.json")
 
 wf.makeWordCloud("tweets_small.json")
