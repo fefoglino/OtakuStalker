dad738d1f7973cc6b438677e239f7132bf9bd3a6
fefoglino <42367586+fefoglino@users.noreply.github.com>
2018-08-20 10:39:52 -0400

    Update otakustalker_mostusedhashtags.py

--- otakustalker_mostusedhashtags.py
+++ otakustalker_mostusedhashtags.py
@@ -2,33 +2,6 @@ import json
 from collections import Counter
 
 
-jsonFile = "tweets_small.json"
-
-def mostCommonHashtags(jsonFile):
-    hashtagList = []
-    mostUsedHashtagsList = []
-
-    tweetFile = open(jsonFile, "r")
-    tweetData = json.load(tweetFile)
-    tweetFile.close()
-
-    emptyList=[]
-
-    for i in range(len(tweetData)):
-        if (tweetData[i]["hashtags"])==emptyList:
-            continue
-        else:
-            for j in range(len(tweetData[i]["hashtags"])):
-                hashtagList.append(tweetData[i]["hashtags"][j]["text"])
-
-    mostUsedHashtags=Counter(hashtagList).most_common(5)
-
-    for i in range(5):
-        mostUsedHashtagsList.append(mostUsedHashtags[i][0])
-        import json
-from collections import Counter
-
-
 
 def mostCommonHashtags(tweetData):
     hashtagList = []
