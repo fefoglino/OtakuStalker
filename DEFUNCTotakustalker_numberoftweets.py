85b265706ed1d22fbf81385da89fc6b687671914
fefoglino <42367586+fefoglino@users.noreply.github.com>
2018-08-21 14:31:27 -0400

    Delete DEFUNCTotakustalker_numberoftweets.py

--- DEFUNCTotakustalker_numberoftweets.py
+++
@@ -1,9 +0,0 @@
-import json
-
-jsonFile = "tweets_small.json"
-
-def numberOfTweets(jsonFile):
-    tweetFile = open(jsonFile, "r")
-    tweetData = json.load(tweetFile)
-    tweetFile.close()
-    return len(tweetData)
